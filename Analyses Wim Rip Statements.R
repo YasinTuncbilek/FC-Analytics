5914e7828fb07734f684ed2f0c354c781201aa13
Yasin Tuncbilek <y_tuncbilek@hotmail.com>
2017-06-05 17:47:54 +0200

    Script updated

--- Analyses Wim Rip Statements.R
+++ Analyses Wim Rip Statements.R
@@ -6,15 +6,6 @@ StatsPSV <- read.csv2("Analysis Wim Rip statements.csv", header = TRUE, sep = ";
 # Read data types
 str(StatsPSV)
 
-# Season 2014/2015 data
-Season1 <- subset(StatsPSV, Season == "2014/2015")
-
-# Season 2015/2016 data
-Season2 <- subset(StatsPSV, Season == "2015/2016")
-
-# Season 2016/2017 data
-Season3 <- subset(StatsPSV, Season == "2016/2017")
-
 # Convert integers to character data type
 StatsPSV[3:4] <- lapply(StatsPSV[3:4], as.character)
 
@@ -26,3 +17,15 @@ StatsPSV[5:11] <- lapply(StatsPSV[5:11], as.numeric)
 
 # Check whether converting was ok
 str(StatsPSV)
+
+# Season 2014/2015 data
+Season1 <- subset(StatsPSV, Season == "2014/2015")
+
+# Season 2015/2016 data
+Season2 <- subset(StatsPSV, Season == "2015/2016")
+
+# Season 2016/2017 data
+Season3 <- subset(StatsPSV, Season == "2016/2017")
+
+# Check structure Season 1 subset
+str(Season1)
