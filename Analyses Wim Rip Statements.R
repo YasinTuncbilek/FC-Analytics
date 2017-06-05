cbf39de778bc39093f2d2b5fe81521aed9bf30f4
Yasin Tuncbilek <y_tuncbilek@hotmail.com>
2017-06-05 16:24:51 +0200

    Data per season

--- Analyses Wim Rip Statements.R
+++ Analyses Wim Rip Statements.R
@@ -5,3 +5,12 @@ StatsPSV <- read.csv2("Analysis Wim Rip statements.csv", header = TRUE, sep = ";
 
 # Read data types
 str(StatsPSV)
+
+# Season 2014/2015 data
+Season1 <- subset(StatsPSV, Season == "2014/2015")
+
+# Season 2015/2016 data
+Season2 <- subset(StatsPSV, Season == "2015/2016")
+
+# Season 2016/2017 data
+Season3 <- subset(StatsPSV, Season == "2016/2017")
