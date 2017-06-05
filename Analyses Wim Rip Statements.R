df646a8dfc6c956496d3e46fac946f7a149dcdb5
Yasin Tuncbilek <y_tuncbilek@hotmail.com>
2017-06-05 15:56:11 +0200

    Last update

--- Analyses Wim Rip Statements.R
+++ Analyses Wim Rip Statements.R
@@ -1,4 +1,7 @@
 # Analyses of Wim Rip (Video-analyst PSV) statements
 
 # Import dataset stats PSV
-StatsPSV <- read.csv2("")
+StatsPSV <- read.csv2("Analysis Wim Rip statements.csv", header = TRUE, sep = ";", dec = ",")
+
+# Read data types
+str(StatsPSV)
